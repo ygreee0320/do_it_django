988c48759ca02c66e957a69212ee9f213ce2ec7f
ygreee <ygreee0320@gmail.com>
2022-09-29 14:41:41 +0900

    CBV로 포스트 목록/상세 페이지 만들기

--- blog/urls.py
+++ blog/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [   # IP주소/blog
-    path('', views.index),  # IP주소/blog
-    path('<int:pk>/', views.single_post_page)
+    path('', views.PostList.as_view()),  #인덱스부분 클래스로 대체
+    path('<int:pk>/', views.PostDetail.as_view())
+    #path('', views.index),  # IP주소/blog
+    #path('<int:pk>/', views.single_post_page)
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,11 +1,23 @@
 from django.shortcuts import render
 from .models import Post
+from django.views.generic import ListView, DetailView
 
 # Create your views here.
-def index(request):
-    posts1 = Post.objects.all().order_by('-pk') # 역순 출력
-    return render (request, 'blog/index.html', {'posts': posts1})
+class PostList(ListView):  #클래스이름:모델명+리스트
+    model = Post
+    ordering = '-pk'
+    # 템플릿 필요, 이름: 모델명_list.html : post_list.html <-자동생성
+    # 파라미터(자동으로 전달되는 데이터) -> 모델명_list : post_list
 
-def single_post_page(request, pk):
-    post2 = Post.objects.get(pk=pk)
-    return render(request, 'blog/single_post_page.html', {'post':post2})
+class PostDetail(DetailView):
+    model = Post
+    # 템플릿 모델명_detail.html : post_detail.html
+    # 파라미터(자동으로 전달되는 데이터) -> 모델명 : post
+
+#def index(request):
+#    posts1 = Post.objects.all().order_by('-pk') # 역순 출력
+#    return render (request, 'blog/index.html', {'posts': posts1})
+
+#def single_post_page(request, pk):
+#    post2 = Post.objects.get(pk=pk)
+#    return render(request, 'blog/single_post_page.html', {'post':post2})
