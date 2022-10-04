4a4b00a7aa575fcf7f4376ab3ad3fc335429ea35
ygreee <ygreee0320@gmail.com>
2022-10-04 17:27:01 +0900

    포스트 목록/상세 페이지에 부트스트랩 적용하기

--- blog/urls.py
+++ blog/urls.py
@@ -4,6 +4,8 @@ from . import views
 urlpatterns = [   # IP주소/blog
     path('', views.PostList.as_view()),  #인덱스부분 클래스로 대체
     path('<int:pk>/', views.PostDetail.as_view())
+
+
     #path('', views.index),  # IP주소/blog
     #path('<int:pk>/', views.single_post_page)
 ]
--- single_pages/views.py
+++ single_pages/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 
 # Create your views here.
 def landing(request):
-    return render(request, 'singel_pages/landing.html')
+    return render(request, 'single_pages/landing.html')
 
 def about_me(request):
     return render(request, 'single_pages/about_me.html')
