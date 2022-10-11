a143b7cc7b2ccd12cfc498fe0feefa2233d13c5b
ygreee <ygreee0320@gmail.com>
2022-10-11 17:37:29 +0900

    포스트 목록/상세 페이지에 첨부파일, 미리보기, 필터 적용하기

--- blog/models.py
+++ blog/models.py
@@ -1,8 +1,10 @@
 from django.db import models
+import os
 
 # Create your models here.
 class Post(models.Model):
     title = models.CharField(max_length=30)
+    hook_text = models.CharField(max_length=100, blank=True) # 데이터 자체가 변경됨->migration필수!
     content = models.TextField()
 
     head_image = models.ImageField(upload_to='blog/images/%Y/%m/%d/', blank=True)  # _media 생략됨 _media/blog/images/
@@ -20,3 +22,11 @@ class Post(models.Model):
 
     def get_absolute_url(self):
         return f'/blog/{self.pk}/'
+
+    def get_file_name(self):
+        return os.path.basename(self.file_upload.name)
+
+    def get_file_ext(self):
+        return self.get_file_name().split('.')[-1]
+        # ~.txt ~~.docx 등을 가져옴 ->스플릿에 의해서 ~ txt / ~~ docx 두개로 쪼개서 배열스트링에 전달
+        # a.b.c.txt -> a b c txt 로 쪼개지면 -> 배열에서 제일 마지막이 확장자임 (제일 마지막 원소 표시 : -1)
