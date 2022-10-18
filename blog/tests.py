c1863bb7795f8dc006f8808159f99f91b23a7281
ygreee <ygreee0320@gmail.com>
2022-10-18 16:51:16 +0900

    포스트 상세 페이지 테스트

--- blog/tests.py
+++ blog/tests.py
@@ -50,3 +50,10 @@ class TestView(TestCase): #클래스 이름: Test로 시작
         navbar = soup.nav
         self.assertIn('Blog', navbar.text)
         self.assertIn('About Me', navbar.text)
+
+        self.assertIn(post_001.title, soup.title.text)
+
+        main_area = soup.find('div', id='main-area')
+        post_area = main_area.find('div', id='post-area')
+        self.assertIn(post_001.title, post_area.text)
+        self.assertIn(post_001.content, post_area.text)
